2e67b8540a4f990b966db61e0abcdebdb0f66cc4
Kim Harold <pinanonangkim000@gmail.com>
2024-01-03 02:10:37 +0800

    Remove rating on skills

--- app/SkillsPage/SkillsPage.tsx
+++ app/SkillsPage/SkillsPage.tsx
@@ -21,7 +21,6 @@ const SkillsPage = () => {
               <CardBody display="flex" flexDirection="column" alignItems="center" gap="4">
                 {skill.icon}
                 <Text>{skill.label}</Text>
-                <Badge colorScheme={skill.color} size="large">{skill.rating}</Badge>
               </CardBody>
             </Card>
         )}
@@ -34,103 +33,86 @@ const skills = [
   {
       icon: <TbBrandNextjs size="48" />, 
       label: "NextJs", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <FaReact size="48" />, 
       label: "ReactJs", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <TbBrandTypescript size="48" />, 
       label: "Typescript", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <IoLogoJavascript size="48" />, 
       label: "Javascript", 
-      rating: "advanced",
       color: "red",
   },
   {
       icon: <SiExpress size="48" />, 
       label: "ExpressJs", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <SiJquery size="48" />, 
       label: "JQuery", 
-      rating: "beginner",
       color: "blue",
   },
   {
       icon: <SiPhp size="48" />, 
       label: "Php", 
-      rating: "beginner",
       color: "blue",
   },
   {
       icon: <SiPrisma size="48" />, 
       label: "Prisma", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <BiLogoMongodb size="48" />, 
       label: "MongoDb", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <TbBrandMysql size="48" />, 
       label: "MySql", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <SiZod size="48" />, 
       label: "Zod", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <SiTailwindcss size="48" />, 
       label: "TailwindCss", 
-      rating: "advanced",
       color: "red",
   },
   {
       icon: <SiChakraui size="48" />, 
       label: "ChakraUi", 
-      rating: "advanced",
       color: "red",
   },
   {
       icon: <SiMui size="48" />, 
       label: "MUi", 
-      rating: "beginner",
       color: "blue",
   },
   {
       icon: <SiRadixui size="48" />, 
       label: "RadixUi", 
-      rating: "intermediate",
       color: "purple",
   },
   {
       icon: <SiDaisyui  size="48" />, 
       label: "DaisyUi", 
-      rating: "advanced",
       color: "red",
   },
   {
       icon: <SiAdobephotoshop  size="48" />, 
       label: "Photoshop", 
-      rating: "advanced",
       color: "red",
   },
 ]
